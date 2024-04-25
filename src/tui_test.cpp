bbb82990f80ff3ebbce476dccd1893bbf76046fa
leofrancoceti <160191116+leofrancoceti@users.noreply.github.com>
2024-04-25 00:03:30 +0000

    version 2

--- src/tui_test.cpp
+++ src/tui_test.cpp
@@ -1,25 +1,63 @@
+#include <ftxui/component/animation.hpp>
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/screen/screen.hpp>
+#include <ftxui/screen/color.hpp>
 #include <iostream>
 #include <string>
+#include <thread>
+#include <experimental/random>
 
 
 using namespace std;
 using namespace ftxui;
+
 int main(int argc, char const *argv[])
 {
-    const string texto = "Hola Mundo";
-    Element textElement = text(texto);
+    int fotograma = 0;
+    string reset;
+
+    while (true)
+    {
+        /* code */
+        int r = std::experimental::randint(0,255);
+        int g = std::experimental::randint(0,255);
+        int b = std::experimental::randint(0,255);
+
+        Element personaje = spinner(21,fotograma);
+
+        //INSTRUCCIONES PARA COLORES ESPECIFICOS
+        //Decorator colorFondo = bgcolor(Color::Blue1);
+        //Decorator colorTexto = color(Color::Red1);*/
+
+        //ISNTRUCCIONES PARA COLORES RANDOMIZADOS RGB
+        Decorator colorFondo = bgcolor(Color::RGB(r,g,b));
+        Decorator colorTexto = color(Color::RGB(b,r,g));
+
+        
+        Element dibujo = border({
+            hbox(personaje) | colorFondo 
+        })| colorTexto ;
+
+
+        Dimensions Alto = Dimension::Fixed(10);
+        Dimensions Ancho = Dimension::Full();
+
+        Screen pantalla = Screen::Create(Ancho,Alto);
+
+        Render(pantalla,dibujo);
+        pantalla.Print();
+        reset = pantalla.ResetPosition();
+        cout<<reset;
+        this
+
+        cout<<reset;
+
+        fotograma++;
+
+        this_thread::sleep_for(0.1s);
+    }
     
-    Element dibujo = hbox(textElement|border );
 
-    Dimensions Alto = Dimension::Fixed(10);
-    Dimensions Ancho = Dimension::Fixed(10);
-
-    Screen pantalla = Screen::Create(Ancho,Alto);
-    Render(pantalla,dibujo);
-    pantalla.Print();
-    cout<<endl;
 
     return 0;
 }
