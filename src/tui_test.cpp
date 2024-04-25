579cce9a8e7526024fd902e14e29eed45fc1b64d
leofrancoceti <160191116+leofrancoceti@users.noreply.github.com>
2024-04-25 00:58:10 +0000

    Version 3

--- src/tui_test.cpp
+++ src/tui_test.cpp
@@ -1,63 +1,66 @@
-#include <ftxui/component/animation.hpp>
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/screen/screen.hpp>
 #include <ftxui/screen/color.hpp>
 #include <iostream>
 #include <string>
 #include <thread>
-#include <experimental/random>
-
+#include <list>
+#include<fstream>
 
 using namespace std;
 using namespace ftxui;
 
 int main(int argc, char const *argv[])
 {
-    int fotograma = 0;
-    string reset;
+    list<string> textos;
+    fstream imagen;
+    imagen.open("./assets/imagen.txt");
 
+    string linea;
+    while (getline(imagen,linea))
+    {
+        textos.push_back(linea);
+    }
+    imagen.close();
+
+    int fotograma = 0;
+    int posX = 0;
+    int posY = 6;
+    string reset;
     while (true)
     {
-        /* code */
-        int r = std::experimental::randint(0,255);
-        int g = std::experimental::randint(0,255);
-        int b = std::experimental::randint(0,255);
+        fotograma++;
 
-        Element personaje = spinner(21,fotograma);
+        Element personaje = spinner(21, fotograma);
+        Decorator colorFondo = bgcolor(Color::DarkBlue);
+        Decorator colorTexto = color(Color::White);
+        Element dibujo = border({hbox(personaje) | colorFondo | colorTexto});
 
-        //INSTRUCCIONES PARA COLORES ESPECIFICOS
-        //Decorator colorFondo = bgcolor(Color::Blue1);
-        //Decorator colorTexto = color(Color::Red1);*/
-
-        //ISNTRUCCIONES PARA COLORES RANDOMIZADOS RGB
-        Decorator colorFondo = bgcolor(Color::RGB(r,g,b));
-        Decorator colorTexto = color(Color::RGB(b,r,g));
-
-        
-        Element dibujo = border({
-            hbox(personaje) | colorFondo 
-        })| colorTexto ;
-
-
-        Dimensions Alto = Dimension::Fixed(10);
+        Dimensions Alto = Dimension::Full();
         Dimensions Ancho = Dimension::Full();
 
-        Screen pantalla = Screen::Create(Ancho,Alto);
+        Screen pantalla = Screen::Create(Ancho, Alto);
 
-        Render(pantalla,dibujo);
+        Render(pantalla, dibujo);
+        int l = 0;
+        for (auto &&texto : textos)
+        {
+            int i = 0;
+            for (auto &&letra : texto)
+            {
+                pantalla.PixelAt(posX + i, posY + l).character = letra;
+                i++;
+            }
+            l++;
+        }
+        posX++;
+
         pantalla.Print();
         reset = pantalla.ResetPosition();
-        cout<<reset;
-        this
-
-        cout<<reset;
-
-        fotograma++;
+        cout << reset;
 
         this_thread::sleep_for(0.1s);
     }
-    
-
 
     return 0;
 }
